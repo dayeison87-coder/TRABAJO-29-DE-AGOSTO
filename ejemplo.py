6ae0750199ce8e2ead5e35287853845b2aa67897
dayeison87-coder <dayeison87@gmail.com>
2025-08-29 13:59:12 -0500

    first commit

---
+++ ejemplo.py
@@ -0,0 +1,25 @@
+lista = ["jose", "miguel", "luis","pedro", "pablo", "ivan", "juan"]
+print(lista[2:5])  
+for nombre in lista:
+    print(nombre)
+
+# Acceder a un rango de elementos de la lista
+lista2 = lista [2:4]
+print(lista2)
+
+# Acceder al último elemento de la lista
+lista2 = lista [-3]
+print(lista2)
+
+# Modificar un elemento de la lista
+lista[6] = "pepe"
+print(lista)
+
+# Agregar un elemento al final de la lista
+lista.append("pedropablo")
+print(lista)
+
+# Insertar un elemento en una posición específica
+lista.insert(1, "maria")
+print(lista)
+
